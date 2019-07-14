72452effc3fbf90086f673b5a2dc2cf535e0d7bc
fabianramos01 <fabian.ramos01@uptc.edu.co>
2019-07-14 16:19:42 -0500

    Carga de productos

--- src/app/models/httpStatus.ts
+++ src/app/models/httpStatus.ts
@@ -2,4 +2,4 @@ export const  OK = 200;
 export const NOT_ACCEPTABLE = 406;
 export const INTERNAL_SERVER_ERROR = 500;
 export const HTTP_URL = 'https://sabbyapp.herokuapp.com';
-//export const HTTP_URL = 'http://localhost:3000';
+// export const HTTP_URL = 'http://localhost:3000';
--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -1,19 +1,19 @@
-import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
-import { HTTP } from '@ionic-native/http/ngx';
-import { Observable } from 'rxjs';
-import {PlatformModel} from '../models/Platform.model';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {HTTP} from '@ionic-native/http/ngx';
+import {Observable} from 'rxjs';
+import {ProductModel} from '../models/Product.model';
 import {HTTP_URL} from '../models/httpStatus';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor(private http2: HTTP,
-              private http: HttpClient) {}
+    constructor(private http: HttpClient) {
+    }
 
-  public getDataProduct(): Observable<PlatformModel[]> {
-    return this.http.get<PlatformModel[]>(HTTP_URL + '/product');
-  }
+    public getDataProduct(): Observable<ProductModel[]> {
+        return this.http.get<ProductModel[]>(HTTP_URL + '/product');
+    }
 }
