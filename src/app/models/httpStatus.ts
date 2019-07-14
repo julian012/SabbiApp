e064cd7dba335ee055af9949c672d3f8d819876c
fabianramos01 <fabian.ramos01@uptc.edu.co>
2019-07-14 16:43:05 -0500

    Add

---
+++ src/app/models/httpStatus.ts
@@ -0,0 +1,11 @@
+export const  OK = 200;
+export const NOT_ACCEPTABLE = 406;
+export const INTERNAL_SERVER_ERROR = 500;
+export const HTTP_URL = 'https://sabbyapp.herokuapp.com';
+<<<<<<< HEAD
+// export const HTTP_URL = 'http://localhost:3000';
+=======
+export const API_DOLLAR_URL = 'https://api.sunat.cloud/cambio';
+export const DATE_FORMAT = 'yyyy-mm-dd';
+//export const HTTP_URL = 'http://localhost:3000';
+>>>>>>> 8e622ed0d5d424f2463a0a600f212393607a0036
